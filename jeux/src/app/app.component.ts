a0b43e6744e7cb346c72d014a0a7f473fabb9870
GIMENEZ Juan <juan.3594@gmail.com>
2024-05-27 15:37:37 +0200

    Mise à jour des fichiers

--- jeux/src/app/app.component.ts
+++ jeux/src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GalerieComponent } from './galerie/galerie.component';
+import { ReseauSocialComponent } from './reseau-social/reseau-social.component';
 import { Jo2024Component } from './jo2024/jo2024.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [GalerieComponent, Jo2024Component, RouterOutlet],
+  imports: [ReseauSocialComponent, Jo2024Component,RouterOutlet],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'JEUX_2024';
-}
+export class AppComponent {}
+  
+
---
+++ jeux/src/app/reseau-social/reseau-social.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Jo2024Component } from '../jo2024/jo2024.component';
+
+@Component({
+  selector: 'app-reseau-social',
+  standalone: true,
+  imports: [Jo2024Component],
+  templateUrl: './reseau-social.component.html',
+  styleUrl: './reseau-social.component.css'
+})
+export class ReseauSocialComponent {
+
+}
